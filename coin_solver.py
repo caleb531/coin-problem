ff7a492a2729391b6694601463e856465f7ec279
Caleb Evans <caleb@calebevans.me>
2021-02-07 17:26:20 -0800

    Revert "Remove redundant int() call"
    This reverts commit 5de39433e9c61280e81212c367b712cd5cf89eeb.

--- coin_solver.py
+++ coin_solver.py
@@ -90,7 +90,7 @@ def perform_adjustment_substitutions(coin_counts, total_coin_amount):
         if round(amount_diff % coin_sum, 2) == 0:
             for coin_type, coin_diff in coin_combination.items():
                 coin_counts[coin_type] += (coin_diff *
-                                           (amount_diff // coin_sum))
+                                           int(amount_diff // coin_sum))
             break
 
 
