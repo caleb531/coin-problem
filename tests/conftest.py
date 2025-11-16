ef0f6722d07cae70f37a955205acd90acef2ac31
Caleb Evans <caleb@calebevans.me>
2025-11-15 17:18:15 -0800

    Fix pytest not finding coinproblem module
    We just needed to run `uv sync` to correct the lockfile's
    package.source value to "editable" instead of "virtual", thus
    eliminating the hacky workaround in conftest.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,6 @@
-import sys
-from pathlib import Path
 from typing import Optional
 
 import pytest
-
-# For some reason, the coinproblem module does not exist on sys.path in the
-# pytest context, so we must add it manually
-PROJECT_ROOT = Path(__file__).resolve().parents[1]
-if str(PROJECT_ROOT) not in sys.path:
-    sys.path.insert(0, str(PROJECT_ROOT))
 
 
 @pytest.hookimpl(tryfirst=True)
